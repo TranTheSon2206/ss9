1f90a8429068bee2ef3eaf22f995321b65dca56b
TranTheSon2206 <109707502+TranTheSon2206@users.noreply.github.com>
2023-05-25 23:09:14 +0700

    Create Catetory.cs

---
+++ Catetory.cs
@@ -0,0 +1,15 @@
+using System;
+public class Catetory
+{
+    string _category;
+
+    public Catetory()
+    {
+        _category = "MultiUtilityVehicle";
+    }
+
+    public void Display()
+    {
+        Console.WriteLine("Category : " + _category);
+    }
+}
