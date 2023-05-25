5d61e95e4e07875c95a98b08bafcb078a7ea0d31
TranTheSon2206 <109707502+TranTheSon2206@users.noreply.github.com>
2023-05-25 23:10:20 +0700

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        SpareParts spObj = new SpareParts();
+        Catetory cateObj = new Catetory();
+
+        spObj.Display();
+        cateObj.Display();
+    }
+}
